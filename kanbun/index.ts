bcdf3c3650b1ce5baf6875b8891b41fb5c3d6f31
halchell <halchell1423@gmail.com>
2025-04-19 01:17:14 +0900

    try~catchでエラーハンドリングを実装

--- kanbun/index.ts
+++ kanbun/index.ts
@@ -6,50 +6,51 @@ const db = new Database("sqlite.db")
 
 initializeItemTable(db);
 
-if(Bun.argv.length === 4){
-  // コマンドライン引数の最後の文字列を取得する
-const content: string = Bun.argv.pop() ?? "";
-const command: string = Bun.argv.pop() ?? "";
+try {
+  if (Bun.argv.length === 4) {
+    // コマンドライン引数の最後2つの文字列を取得する
+    const content: string = Bun.argv.pop() ?? "";
+    const command: string = Bun.argv.pop() ?? "";
 
-switch(command){
-  case "memo":
-    createItem(db, content, "memo")
-    break;
-  case "todo":
-    createItem(db, content, "todo")
-    break;
-  case "done":
-    updateTodoToDone(db, content);
-    break;
-  case "drop":
-    deleteItem(db, content);
-    break;
-  default:
-    throw new Error("不正なコマンドです");
-}
+    switch (command) {
+      case "memo":
+        createItem(db, content, "memo");
+        break;
+      case "todo":
+        createItem(db, content, "todo");
+        break;
+      case "done":
+        updateTodoToDone(db, content);
+        break;
+      case "drop":
+        deleteItem(db, content);
+        break;
+      default:
+        throw new Error("不正なコマンドです");
+    }
 
-printItems(db);
+    printItems(db);
+  } else if (Bun.argv.length === 3) {
+    const command: string = Bun.argv.pop() ?? "";
 
-} else if (Bun.argv.length === 3) {
-  const command: string = Bun.argv.pop() ?? "";
+    switch (command) {
+      case "trim":
+        archiveItems(db);
+        break;
+      default:
+        throw new Error("不正なコマンドです");
+    }
 
-  switch (command) {
-    case "trim":
-      archiveItems(db);
-      break;
-    default:
-      throw new Error("不正なコマンドです");
+    printItems(db);
+  } else if (Bun.argv.length === 2) {
+    printItems(db);
+  } else {
+    throw new Error("コマンドライン引数が多すぎます");
   }
-
-  printItems(db);
-
-} else if(Bun.argv.length === 2){
-  const items = getItems(db);
-  items.forEach((item) => {
-    console.log(formatToItem(item));
-  })
-} else{
-  throw new Error("追加のコマンドライン引数は1つまでです。");
+} catch (error) {
+  if (error instanceof Error) {
+    console.error(error.message);
+  }
 }
 
 db.close();
