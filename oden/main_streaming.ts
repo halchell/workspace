b480f32e54acbcf028f2ddee4779f249fb877302
halchell <halchell1423@gmail.com>
2025-04-23 08:11:04 +0900

    1秒ごとに現在時刻を送信し続けるHTTPストリーミングサーバーを実装

---
+++ oden/main_streaming.ts
@@ -0,0 +1,19 @@
+function handler(_req: Request): Response {
+  let timer: number | undefined = undefined;
+  const body = new ReadableStream({
+    start(controller) {
+      timer = setInterval(() => {
+        const message = `It is ${new Date().toISOString()}\n`;
+        controller.enqueue(new TextEncoder().encode(message));
+      }, 1000);
+    },
+  });
+  return new Response(body, {
+    headers: {
+      "content-type": "text/plain",
+      "x-content-type-options": "nosniff",
+    },
+  });
+}
+
+Deno.serve(handler);
