1c034cf1f049f7a5464dddffe5e5f46fd398ff06
halchell <halchell1423@gmail.com>
2025-04-24 20:19:55 +0900

    deno.land/xにあるopenaiのmoduleを参照するように

--- oden/main.ts
+++ oden/main.ts
@@ -1,12 +1,1 @@
-async function handler() {
-  const html = await Deno.readTextFile("./index.html");
-  const response = new Response(html, {
-    headers: {
-      "content-type": "text/html;charset=utf-8",
-    },
-  });
-
-  return response;
-}
-
-Deno.serve(handler);
+import OpenAI from "https://deno.land/x/openai@v4.62.1/mod.ts";
