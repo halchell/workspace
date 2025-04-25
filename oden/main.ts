bc5afcd2407ae697001b38973340b0742c620e47
halchell <halchell1423@gmail.com>
2025-04-25 21:46:24 +0900

    ページにリクエストが来るとChatGPT APIのchat completionが実行されるように実装

--- oden/main.ts
+++ oden/main.ts
@@ -1,5 +1,28 @@
 import OpenAI from "https://deno.land/x/openai@v4.62.1/mod.ts";
+import { Completions } from "https://deno.land/x/openai@v4.62.1/resources/mod.ts";
 
 const openai = new OpenAI({
   apiKey: Deno.env.get("OPENAI_API_KEY"),
 });
+
+async function handler(){
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {role: "system", content: "レシピ作成アシスタント"},
+      {role: "user", content: "おでん"} ,
+    ],
+    model: "gpt-4o-mini",
+  });
+
+  const content = completion.choices[0].message.content;
+
+  const response = new Response(content,{
+    headers: {
+      "content-type": "text/plain;charset=utf-8",
+    },
+  });
+
+  return response;
+}
+
+Deno.serve(handler);
