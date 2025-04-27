be4e834b6ae5c94a476064c236b7addbace3954a
halchell <halchell1423@gmail.com>
2025-04-27 12:04:35 +0900

    クエリパラメータの値を返すAPIを実装

--- deno-hono-api/main.ts
+++ deno-hono-api/main.ts
@@ -1,9 +1,10 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/api/title', (c) => {
+  const url = c.req.query("url") ?? "";
+  return c.json<{ url: string }>({ url });
 })
 
-Deno.serve(app.fetch)
+Deno.serve(app.fetch);
