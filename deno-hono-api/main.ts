ad6523ac8eeae0d599f9b45f6d4b79ab6936b058
halchell <halchell1423@gmail.com>
2025-04-27 12:38:14 +0900

    HTMLをDOMツリーにパースしてtitle要素の中身を取り出す処理を実装

--- deno-hono-api/main.ts
+++ deno-hono-api/main.ts
@@ -1,10 +1,21 @@
 import { Hono } from 'hono';
+import { DOMParser } from "@b-fuze/deno-dom"
 
 const app = new Hono()
 
-app.get('/api/title', (c) => {
+app.get('/api/title', async (c) => {
   const url = c.req.query("url") ?? "";
-  return c.json<{ url: string }>({ url });
-})
+
+  const res = await fetch(url);
+  const html = await res.text();
+
+  const parser = new DOMParser();
+  const document = parser.parseFromString(html, "text/html");
+
+  const titleElement = document.querySelector("title");
+  const title = titleElement?.textContent;
+
+  return c.json<{ url: string; title: string }>({ url, title });
+});
 
 Deno.serve(app.fetch);
