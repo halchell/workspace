5bda3aafb830afe24daf492e4d4d7fa26645d6e4
halchell <halchell1423@gmail.com>
2025-05-07 22:50:15 +0900

    国情報表示用のテンプレートを実装

--- nodemonde/src/components/Card.vue
+++ nodemonde/src/components/Card.vue
@@ -5,3 +5,20 @@ defineProps<{
   tldList: string[];
 }>();
 </script>
+
+<template>
+  <article>
+    <picture>
+      <img :src="image" alt="flag" />
+    </picture>
+    <div>
+      <h2>{{ name }}</h2>
+      <dl>
+        <dt>Top-level domains</dt>
+        <dd v-for="tld in tldList">
+          {{ tld }}
+        </dd>
+      </dl>
+    </div>
+  </article>
+</template>
