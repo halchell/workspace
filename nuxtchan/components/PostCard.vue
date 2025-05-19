d590a292f293f3863e679337e8f0676ad88c28d3
halchell <halchell1423@gmail.com>
2025-05-19 21:10:06 +0900

    ポストに関する箇所のマークアップをPostCardコンポーネントになるように移す

---
+++ nuxtchan/components/PostCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <UCard>
+    <p>
+      <span class="font-bold text-green-600">名無しさん</span>
+      <span>：</span>
+      <NuxtLink to="/abc123" class="underline">
+        2025-05-19T12:34:56.00Z
+      </NuxtLink>
+    </p>
+
+    <UContainer>
+      <p class="whitespace-break-spaces">本文テキスト</p>
+    </UContainer>
+  </UCard>
+</template>
