12831be7112e99726ca39e183f8f78dd46f5b164
halchell <halchell1423@gmail.com>
2025-05-20 23:03:06 +0900

    入力フォームに関する箇所のマークアップをPostFormコンポーネントになるように移す

---
+++ nuxtchan/components/PostForm.vue
@@ -0,0 +1,9 @@
+<template>
+  <UContainer class="text-right w-full">
+    <form>
+      <UInput placeholder="お名前" class="w-40" />
+      <UTextarea placeholder="本文" />
+      <UButton>投稿</UButton>
+    </form>
+  </UContainer>
+</template>
