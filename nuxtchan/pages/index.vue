a7611a1b75c0ebde624d564f9014aa0838f44dd1
halchell <halchell1423@gmail.com>
2025-05-21 00:13:16 +0900

    index.vueからPostCard,PostFormコンポーネントを呼び出すように修正

--- nuxtchan/pages/index.vue
+++ nuxtchan/pages/index.vue
@@ -1,23 +1,9 @@
 <template>
   <main class="flex flex-col gap-4">
-    <UContainer class ="text-right">
-      <form>
-        <UInput placeholder="お名前" class="w-40" />
-        <UTextarea placeholder="本文" />
-        <UButton>投稿</UButton>
-      </form>
-    </UContainer>
+    <PostForm />
 
-    <UCard>
-      <p>
-        <span class="font-bold text-green-600">名無しさん</span>
-        <span>:</span>
-        <Nuxtlink to="/abc123" class="underline">2025年5月18日 日曜日 15:11</Nuxtlink>
-      </p>
-
-      <UContainer>
-        <p class="whitespace-break-spaces">本文テキスト</p>
-      </UContainer>
-    </UCard>
+    <PostCard />
+    <PostCard />
+    <PostCard />
   </main>
 </template>
